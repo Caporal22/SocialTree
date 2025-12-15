8975221e9220e10670675ca41331f9a9c87942e6
DanieliniCaporal <dasacaporal@gmail.com>
2025-12-14 19:46:37 -0600

    feat(auth): implement basic login endpoint and request handling

--- devtree/src/router.ts
+++ devtree/src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import {body} from 'express-validator';
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 /** Auth and registration */
-router.post('/auth/register', createAccount);
+router.post('/auth/register',
+    body('handle').notEmpty().withMessage('Handle is required'),
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Email not valid'),
+    body('password').isLength({min: 8}).withMessage('Password is too short, min 8 characters'),
+    createAccount);
 
 export default router;
