0744f007be37097f88e9b702caf152f8f8faad39
DanieliniCaporal <dasacaporal@gmail.com>
2025-12-16 23:30:16 -0600

    feat(frontend): add auth views, layouts and routing setup

--- devtree/frontend/src/views/LoginView.tsx
+++ devtree/frontend/src/views/LoginView.tsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 export default function LoginView() {
   return (
         <>
-            <div className="text-6xl">LoginView</div>
-            <br></br>
-            <nav>
-                <Link to="/auth/register">Don't have account -- Go to Register</Link>
+    
+            <h1 className='text-4xl text-white font-bold'>Iniciar sesión</h1>
+            <nav className='mt-10'>
+                <Link 
+                    className='text-center text-white text-lg block'
+                    to="/auth/register"
+                >Don't have account -- Go to Register</Link>
             </nav>
         </>
     
